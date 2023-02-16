de0509686caad634f6d2dad01977649a2da4dd70
rrez2002 <rrez2002@gmail.com>
2023-02-16 10:15:32 +0330

    feat add auth swagger

--- src/router/auth.router.ts
+++ src/router/auth.router.ts
@@ -5,7 +5,114 @@ import {Auth} from "../app/http/middlewares/Auth";
 import {validationRequest} from "../app/http/middlewares/ValidationRequest";
 
 const authRouter: Router = Router()
+/**
+ * @swagger
+ * /auth/register:
+ *      post:
+ *          summary: Register User
+ *          tags:
+ *              - Auth
+ *          description: Send User Information for  Registration
+ *          consumes:
+ *              - application/x-www-form-urlencoded
+ *          parameters:
+ *             - name: first_name
+ *               in: formData
+ *               required: true
+ *               type: string
+ *
+ *             - name: last_name
+ *               in: formData
+ *               required: true
+ *               type: string
+ *
+ *             - name: user_name
+ *               description: unique username
+ *               in: formData
+ *               required: true
+ *               type: string
+ *
+ *             - name: phone
+ *               description: IR phone number
+ *               in: formData
+ *               required: true
+ *               type: string
+ *
+ *             - name: password
+ *               description: password
+ *               in: formData
+ *               required: true
+ *               type: string
+ *          requestBody:
+ *              required: true
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              responseText:
+ *                                  type: string
+ *                                  example: This is some example string! This is an endpoint
+ *          responses:
+ *              201:
+ *                  description: Success
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  token:
+ *                                      type: string
+ *              400:
+ *                  description: Bad Request
+ *
+ */
 authRouter.post("/register", RegisterValidator(), validationRequest, AuthController.Register)
+/**
+ * @swagger
+ * /auth/login:
+ *      post:
+ *          summary: Register User
+ *          tags:
+ *              - Auth
+ *          description: Send User Information for  Registration
+ *          consumes:
+ *              - application/x-www-form-urlencoded
+ *          parameters:
+ *             - name: user_name
+ *               description: unique username
+ *               in: formData
+ *               required: true
+ *               type: string
+ *             - name: password
+ *               description: password
+ *               in: formData
+ *               required: true
+ *               type: string
+ *          requestBody:
+ *              required: true
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              responseText:
+ *                                  type: string
+ *                                  example: This is some example string! This is an endpoint
+ *          responses:
+ *              200:
+ *                  description: Success
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  token:
+ *                                      type: string
+ *              400:
+ *                  description: Bad Request
+ *
+ */
 authRouter.post("/login", LoginValidator(), validationRequest, AuthController.Login)
 authRouter.post("/logout", Auth, AuthController.Logout)
 
