3302366ee5dacc79b52605d1c2b26057a2448b17
rrez2002 <rrez2002@gmail.com>
2023-02-21 18:26:50 +0330

    feat DonationLink swagger

---
+++ src/router/swagger/donation.link.swagger.ts
@@ -0,0 +1,195 @@
+/**
+ * @swagger
+ *  components:
+ *      schemas:
+ *          StoreDonationLink:
+ *              type: object
+ *              required:
+ *                  -   link
+ *              properties:
+ *                  link:
+ *                      type: string
+ *                      example:
+ *                  amount :
+ *                      type: string
+ *                      example:
+ *          UpdateDonationLink:
+ *              type: object
+ *              properties:
+ *                  link:
+ *                      type: string
+ *                      example:
+ *                  amount :
+ *                      type: string
+ *                      example:
+ *
+ */
+
+/**
+ * @swagger
+ *  definitions:
+ *      DonationLinks:
+ *          type: array
+ *          items:
+ *              type: object
+ *              properties:
+ *                  _id:
+ *                      type: string
+ *                      example:
+ *                  link:
+ *                      type: string
+ *                      example:
+ *                  amount :
+ *                      type: string
+ *                      example:
+ *
+ *      DonationLink:
+ *          type: object
+ *          properties:
+ *             _id:
+ *                  type: string
+ *                  example:
+ *             link:
+ *                  type: string
+ *                  example:
+ *             amount :
+ *                  type: string
+ *                  example:
+ *      StoreDonationLink:
+ *          type: object
+ *          properties:
+ *             message:
+ *                  type: string
+ *                  example: create donation_link success
+ *      UpdateDonationLink:
+ *          type: object
+ *          properties:
+ *             message:
+ *                  type: string
+ *                  example: update donation_link success
+ *      DeleteDonationLink:
+ *          type: object
+ *          properties:
+ *             message:
+ *                  type: string
+ *                  example: delete donation_link success
+ *
+ */
+
+
+/**
+ * @swagger
+ *  /donation_link:
+ *      get:
+ *          tags: [DonationLink]
+ *          summary: get donation links
+ *          responses:
+ *              200:
+ *                  description: Success
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              $ref: '#/definitions/DonationLinks'
+ *              400:
+ *                  description: Bad Request
+ *
+ *
+ */
+
+/**
+ * @swagger
+ *  /donation_link:
+ *      post:
+ *          tags: [DonationLink]
+ *          summary: create donation link
+ *          requestBody:
+ *              content:
+ *                  application/x-www-form-urlencoded:
+ *                      schema:
+ *                          $ref: '#/components/schemas/StoreDonationLink'
+ *          responses:
+ *              200:
+ *                  description: Success
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              $ref: '#/definitions/StoreDonationLink'
+ *              400:
+ *                  description: Bad Request
+ */
+
+
+/**
+ * @swagger
+ *  /donation_link/{id}:
+ *      get:
+ *          tags: [DonationLink]
+ *          summary: get donation link by ObjectId
+ *          parameters:
+ *              -   in: path
+ *                  name: id
+ *                  type: string
+ *          responses:
+ *              200:
+ *                  description: Success
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              $ref: '#/definitions/DonationLink'
+ *              400:
+ *                  description: Bad Request
+ *
+ *
+ */
+
+
+/**
+ * @swagger
+ *  /donation_link/{id}:
+ *      patch:
+ *          tags: [DonationLink]
+ *          summary: update donation link by ObjectId
+ *          parameters:
+ *              -   in: path
+ *                  name: id
+ *                  type: string
+ *          requestBody:
+ *              content:
+ *                  schema:
+ *                      $ref: '#/definitions/DonationLink'
+ *          responses:
+ *              200:
+ *                  description: Success
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              $ref: '#/definitions/UpdateDonationLink'
+ *              400:
+ *                  description: Bad Request
+ *
+ *
+ */
+
+
+/**
+ * @swagger
+ *  /donation_link/{id}:
+ *      delete:
+ *          tags: [DonationLink]
+ *          summary: delete donation link by ObjectId
+ *          parameters:
+ *              -   in: path
+ *                  name: id
+ *                  type: string
+ *          responses:
+ *              200:
+ *                  description: Success
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              $ref: '#/definitions/DeleteDonationLink'
+ *              400:
+ *                  description: Bad Request
+ *
+ *
+ */
