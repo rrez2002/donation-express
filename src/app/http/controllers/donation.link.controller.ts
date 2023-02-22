d70547b43cc61bca5b911db64f6c8d13795833a7
rrez2002 <rrez2002@gmail.com>
2023-02-22 11:31:59 +0330

    fix donationLink response

--- src/app/http/controllers/donation.link.controller.ts
+++ src/app/http/controllers/donation.link.controller.ts
@@ -4,8 +4,10 @@ import {Request, Response} from "express";
 class DonationLinkController {
     async Index(req: Request, res: Response): Promise<Response> {
         const user_id = req.user._id
-        const donationLinks: DonationLink[] = await DonationLinkModel.find({user_id})
-        console.log(donationLinks)
+        const donationLinks: DonationLink[] = await DonationLinkModel.find({user_id},{
+            link: 1,
+            amount: 1
+        })
 
         return res.json(donationLinks)
     }
@@ -14,7 +16,10 @@ class DonationLinkController {
         const user_id = req.user._id
         const link_id = req.params.id
 
-        const donationLink = await DonationLinkModel.findOne({user_id,_id:link_id})
+        const donationLink = await DonationLinkModel.findOne({user_id,_id:link_id},{
+            link: 1,
+            amount: 1
+        })
 
         if (donationLink){
             return res.status(200).json(donationLink);
@@ -35,26 +40,35 @@ class DonationLinkController {
                 amount
             })
 
-            return res.status(201).json({})
+            return res.status(201).json({
+                message: "create donation_link success"
+            });
         } catch (e) {
             return res.status(400).json(e)
         }
     }
 
     async Update(req: Request, res: Response): Promise<Response> {
-        return res.json({})
+        const user_id = req.user._id
+        const link_id = req.params.id
+        const body = req.body
+
+        await DonationLinkModel.updateOne({user_id,_id:link_id},{body});
+
+        return res.status(200).json({
+            message: "update donation_link success"
+        });
     }
 
     async Destroy(req: Request, res: Response): Promise<Response> {
+        const user_id = req.user._id
         const link_id = req.params.id
 
-        const donationLink = await DonationLinkModel.deleteOne({_id:link_id})
+        await DonationLinkModel.deleteOne({user_id,_id:link_id})
 
-        if (donationLink.deletedCount){
-            return res.status(200).json({});
-        }
-
-        return res.status(404).json({})
+        return res.status(200).json({
+            message: "delete donation_link success"
+        });
     }
 }
 
