b0789d465dae5bccb23314fe6c53b04008e7fa50
rrez2002 <rrez2002@gmail.com>
2023-02-25 23:46:21 +0330

    feat payment model

---
+++ src/app/models/payment.model.ts
@@ -0,0 +1,35 @@
+import {Model, model, Schema, Types} from "mongoose";
+
+enum PaymentEnum {
+    Success = "success",
+    Failed = "failed",
+    Pending = "pending"
+}
+
+interface IPayment {
+    user_id: Types.ObjectId,
+    amount: number,
+    description: string,
+    validCardNumber: string,
+    status: PaymentEnum
+}
+
+interface IPaymentMethod {
+
+}
+
+
+export type Payment = IPayment & IPaymentMethod
+
+type PaymentModel = Model<IPayment, {}, IPaymentMethod>;
+
+const PaymentSchema = new Schema<IPayment, PaymentModel, IPaymentMethod>({
+    user_id: {type: Schema.Types.ObjectId, ref: "User", required: true},
+    amount: {type: Schema.Types.Number, min: 1},
+    description: {type: Schema.Types.String},
+    validCardNumber: {type: Schema.Types.String, enum: PaymentEnum},
+}, {
+    timestamps: true
+});
+
+export const PaymentModel: PaymentModel = model<IPayment, PaymentModel>("Payment", PaymentSchema);
