7440579fc02c34fd98b58f7c7f7c4444867f76db
rrez2002 <rrez2002@gmail.com>
2023-03-14 22:31:53 +0330

    fix auth dto

--- src/app/http/dtos/auth.dto.ts
+++ src/app/http/dtos/auth.dto.ts
@@ -4,10 +4,7 @@ type RegisterDTO = Omit<IUser, "_id">;
 
 type LoginDTO = Pick<IUser, "user_name" | "password">;
 
-type RefreshTokenDTO = string;
-
 export {
     RegisterDTO,
     LoginDTO,
-    RefreshTokenDTO,
 }
