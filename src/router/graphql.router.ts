9f1ac98e65bb8f5e1670d8e5fe340d9c6afdb976
rrez2002 <rrez2002@gmail.com>
2023-03-20 22:55:39 +0330

    fix graphql router

--- src/router/graphql.router.ts
+++ src/router/graphql.router.ts
@@ -1,14 +1,17 @@
 import {graphqlHTTP} from "express-graphql";
-import {graphQLSchema} from "../app/graphql/index.resolver";
+import {graphQLSchema} from "../app/graphql/index.graphql";
 import {Router} from "express";
 
 const GraphqlRouter: Router = Router()
 
-GraphqlRouter.use("",graphqlHTTP({
+GraphqlRouter.use("",graphqlHTTP((request, response, graphQLParams) => ({
     schema: graphQLSchema,
     graphiql: true,
-    context: {req: Request, res: Response},
-
-}))
+    context: {
+        request,
+        response,
+        graphQLParams
+    },
+})))
 
 export default GraphqlRouter;
