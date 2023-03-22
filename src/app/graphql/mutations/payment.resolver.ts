8c93aa464beb7d69a92bb7f6b8cfd6fefe1a97e3
rrez2002 <rrez2002@gmail.com>
2023-03-23 00:02:27 +0430

    fix gateway url

--- src/app/graphql/mutations/payment.resolver.ts
+++ src/app/graphql/mutations/payment.resolver.ts
@@ -27,27 +27,40 @@ export const gatewayResolver = {
         gateway: {type: GraphQLString},
     },
     resolve: async (parent: any, args: GatewayDTO & {gateway: string, name: string, link: string}) => {
+        try {
+            const {amount, gateway, description, phone, name} = args
 
-        const {amount, gateway, description, phone, name} = args
+            const linkModel = await donationLinkService.FindByLink({
+                link: args.link
+            })
 
-        const linkModel = await donationLinkService.FindByLink({
-            link: args.link
-        })
+            if (!linkModel) return {
+                message: "donation_link not found"
+            }
 
-        if (!linkModel) return {
-            message: "donation_link not found"
+            const result = await PaymentService.Gateway({
+                amount, phone: phone, description
+            }, await getGateway(gateway as GatewayEnum))
+
+            await PaymentService.SaveTransaction({
+                name, phone, amount, authority: result.token, description,
+            })
+
+            return {
+                method: "GET", url: generateUrl(result.token, gateway)
+            };
+        }catch (e) {
+            return (e as Error).message
         }
-
-        const result = await PaymentService.Gateway({
-            amount, phone: phone, description
-        }, await getGateway(gateway as GatewayEnum))
-
-        await PaymentService.SaveTransaction({
-            name, phone, amount, authority: result.token, description,
-        })
-
-        return {
-            method: "GET", url: result.token, gateway
-        };
     }
 };
+
+function generateUrl(gateway:GatewayEnum,authority:string): string {
+    switch (gateway) {
+        case GatewayEnum.PAYIR:
+            return `https://pay.ir/pg/${authority}`
+        case GatewayEnum.ZARINPAL: {
+            return `https://www.zarinpal.com/pg/StartPay/${authority}`
+        }
+    }
+}
